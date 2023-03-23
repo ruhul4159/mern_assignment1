c3344b625a267faed08e7446dd39d08a162e873e
ruhulamin87 <ruhul4159@gmail.com>
2023-03-23 23:13:43 +0300

    livecode test

---
+++ test.js
@@ -0,0 +1,16 @@
+function gradeCalculation(grade) {
+    if (typeof grade !== "number" || grade < 0 || grade > 100) {
+      return "Error: Input must be a number between 0 and 100.";
+    } else if (grade >= 90) {
+      return "A";
+    } else if (grade >= 80) {
+      return "B";
+    } else if (grade >= 70) {
+      return "C";
+    } else if (grade >= 60) {
+      return "D";
+    } else {
+      return "F";
+    }
+  }
+  console.log(gradeCalculation(99));
